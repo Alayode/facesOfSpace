612eaa57e8988743ab924617823ad58e9cdf5b3c
Chris Samuel <ksamuel.chris@icloud.com>
2015-10-07 09:57:21 -0400

    used commit d5519c0

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
 
 /*
-* Chris Samuel
-* ksamuel.chris@icloud.com
-*
-* October 1, 2015
-*
-* FileName: server.js
-*
-* Description:
-*
-* File will be used for running node/express for our voting application.
-* Chris Samuel
-* ksamuel.chris@gmail.com
-*
-* October,
-*
-*
-* */
+ * Chris Samuel
+ * ksamuel.chris@icloud.com
+ *
+ * October 1, 2015
+ *
+ * FileName: server.js
+ *
+ * Description:
+ *
+ * File will be used for running node/express for our voting application.
+ * Chris Samuel
+ * ksamuel.chris@gmail.com
+ *
+ * October,
+ *
+ *
+ * */
 
 //React Routes
 
@@ -45,55 +45,16 @@ juice.use(express.static(path.join(__dirname,'public')));
 
 // Express middleware  components
 // WILL BE EXECUTED ON EVERY REQUEST TO THE SERVER.
-juice.use(function(req, res) {
-    Router.run(routes, req.path, function(Handler) {
+juice.use(function(req,res){
+    Router.run(routes, req.path,function(Handler){
         var html = React.renderToString(React.createElement(Handler));
-        var page = swig.renderFile('views/index.html', { html: html });
+        var page = swig.renderFile('views/index.html',{html:html});
         res.send(page);
     });
 });
 
 
-//  TODO: Replace this block of code for socket.IO
-//juice.listen(juice.get('port'),function(){
-//    console.log('Express server is using the JUICE on port ' + juice.set('port'));
-//});
-
-/**
- * Socket.io stuff.
- */
-var server = require('http').createServer(juice);
-var io = require('socket.io')(server);
-var onlineUsers = 0;
-
-io.sockets.on('connection', function(socket) {
-    onlineUsers++;
-
-    io.sockets.emit('onlineUsers', { onlineUsers: onlineUsers });
-
-    socket.on('disconnect', function() {
-        onlineUsers--;
-        io.sockets.emit('onlineUsers', { onlineUsers: onlineUsers });
-    });
+  //TODO: Replace this block of code for socket.IO
+juice.listen(juice.get('port'),function(){
+    console.log('Express server is using the JUICE on port ' + juice.set('port'));
 });
-
-server.listen(juice.get('port'), function() {
-    console.log('Express server listening on port ' + juice.get('port'));
-});
-/*
-* Explanation for refactor with socket.io
-*
-* WebSocket connections are established. With this block of code
-* when it does, the onlineUsers count will increase by 1 (This is a
-* global variable )and a message is then broadcasts
-*
-* " Hey I have this many users right now ."
-*
-* When someone closes a connection the count is then decreased by one
-* This will then run another message stating hey I have this many online users now.
-*
-*
-*
-*
-*
-* */
