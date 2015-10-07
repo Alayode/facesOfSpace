b1858150e7dfbe4e7822aabbe8110bde7e8c433f
Chris Samuel <ksamuel.chris@icloud.com>
2015-10-07 10:25:15 -0400

    created React Component and constructor object

--- app/components/AddCharacter.js
+++ app/components/AddCharacter.js
@@ -22,7 +22,44 @@ import React from 'react';
 import AddCharacterStore from '../stores/AddCharacterStore';
 import AddCharacterActions from '../actions/AddCharacterActions';
 
+class AddCharacter extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = AddCharacterStore.getState();
+        this.onChange = this.onChange.bind(this);
+    }
 
+    componentDidMount() {
+        AddCharacterStore.listen(this.onChange);
+    }
+
+    componentWillUnmount() {
+        AddCharacterStore.unlisten(this.onChange);
+    }
+
+    onChange(state) {
+        this.setState(state);
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+
+        var name = this.state.name.trim();
+        var gender = this.state.gender;
+
+        if (!name) {
+            AddCharacterActions.invalidName();
+            this.refs.nameTextField.getDOMNode().focus();
+        }
+
+        if (!gender) {
+            AddCharacterActions.invalidGender();
+        }
+
+        if (name && gender) {
+            AddCharacterActions.addCharacter(name, gender);
+        }
+    }
 
 
 export default AddCharacter;
