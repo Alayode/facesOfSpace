669ab2213daf61e149bc0e2cccef7fff29501d19
Chris Samuel <ksamuel.chris@icloud.com>
2015-10-07 10:23:53 -0400

    added files to git

---
+++ app/components/AddCharacter-compiled.js
@@ -0,0 +1,43 @@
+
+/*
+ * Chris Samuel
+ * ksamuel.chris@icloud.com
+ *
+ * October 1, 2015
+ *
+ * FileName: server.js
+ *
+ * Description:
+ *
+ * This components consists of a simple form with a text field, radio buttons and a submit button .
+ * Success and error messages will be displayed within belp-block under the text field.
+ *
+ *
+ *
+ *
+*/
+
+'use strict';
+
+Object.defineProperty(exports, '__esModule', {
+  value: true
+});
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _storesAddCharacterStore = require('../stores/AddCharacterStore');
+
+var _storesAddCharacterStore2 = _interopRequireDefault(_storesAddCharacterStore);
+
+var _actionsAddCharacterActions = require('../actions/AddCharacterActions');
+
+var _actionsAddCharacterActions2 = _interopRequireDefault(_actionsAddCharacterActions);
+
+exports['default'] = AddCharacter;
+module.exports = exports['default'];
+
+//# sourceMappingURL=AddCharacter-compiled.js.map
---
+++ app/components/AddCharacter.js
@@ -0,0 +1,28 @@
+
+/*
+ * Chris Samuel
+ * ksamuel.chris@icloud.com
+ *
+ * October 1, 2015
+ *
+ * FileName: server.js
+ *
+ * Description:
+ *
+ * This components consists of a simple form with a text field, radio buttons and a submit button .
+ * Success and error messages will be displayed within belp-block under the text field.
+ *
+ *
+ *
+ *
+*/
+
+
+import React from 'react';
+import AddCharacterStore from '../stores/AddCharacterStore';
+import AddCharacterActions from '../actions/AddCharacterActions';
+
+
+
+
+export default AddCharacter;
