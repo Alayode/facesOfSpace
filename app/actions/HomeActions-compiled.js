7eac478dd2a5d74da30fdf577e215bf236845fea
Chris Samuel <ksamuel.chris@icloud.com>
2015-10-07 21:49:57 -0400

    created a new file called HomeActions.js  within the actions directory

---
+++ app/actions/HomeActions-compiled.js
@@ -0,0 +1,17 @@
+'use strict';
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+
+var _alt = require('../alt');
+
+var _alt2 = _interopRequireDefault(_alt);
+
+var HomeActions = function HomeActions() {
+    _classCallCheck(this, HomeActions);
+
+    this.generateAct;
+};
+
+//# sourceMappingURL=HomeActions-compiled.js.map
