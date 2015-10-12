43547ab7236f66cfdc4fc3c7bcd87790bb4af071
Chris Samuel <ksamuel.chris@icloud.com>
2015-10-12 16:44:51 -0400

    fixed issue where HomeStore reference was not found during server-start added underscore tools

--- app/components/home.js
+++ app/components/home.js
@@ -1,6 +1,18 @@
 import React from 'react';
+import {Link} from 'react-router';
+import HomeStore from '../stores/HomeStore';
+import HomeActions from  '../actions/HomeActions';
+import {first,without,findWhere} from 'underscore';
+
 
 class Home extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state= HomeStore.getState();
+        this.onChange = this.onChange.bind(this);
+    }
+
     render() {
         return (
             <div className='alert alert-info'>
